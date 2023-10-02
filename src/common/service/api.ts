1de199b41f421f4bd2acd0b47e68d35c9159043c
Guilherme Luiz Lange <gui.luizlange@gmail.com>
2023-10-02 17:04:29 -0300

    fix: public env

--- src/common/service/api.ts
+++ src/common/service/api.ts
@@ -3,7 +3,7 @@ import { getAPIClient } from "./axios";
 
 export const api = getAPIClient()
 
-export const resources = (process.env.API_URL || 'http://127.0.0.1:3000') + '/static/'
+export const resources = (process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:3000') + '/static/'
 
 export default function setTokenApi(token: any) {
     api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
--- src/common/service/axios.ts
+++ src/common/service/axios.ts
@@ -1,13 +1,11 @@
 import axios from "axios"
 import { parseCookies } from "nookies";
-import { Cookies } from 'react-cookie';
-const cookies = new Cookies();
 
 export function getAPIClient(ctx?: any) {
   const { 'nextauth.token': token } = parseCookies(ctx)
-  
+
   const api = axios.create({
-    baseURL: process.env.API_URL || 'http://127.0.0.1:3000'
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:3000'
   })
 
   api.interceptors.request.use(config => {
